ff9d57e486159c3b04ebe3fc7e544761f996760d
shahenaz Monia <shahy.m.93@gmail.com>
2017-06-13 14:53:13 +0300

    make file structure

---
+++ src/backEnd/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const PORT= process.env.PORT || 8080
+
+const app = express();
+const routes = require('./routes.js');
+app.use(express.static('public'));
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log('Our app is running on http://localhost:' + PORT); // eslint-disable-line no-console
+});
---
+++ src/frontEnd/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//import reactDOM from 'react-dom';
+// import store from './store.js';
+// import {Provider} from 'react-redux';
+//import App from './components/containers/App.js';
+
+const Main = () => {
+  return (
+      <div>
+      <h1>hi</h1>
+      </div>
+
+  );
+};
+
+reactDOM.render(
+  <Main/>, document.getElementById('myApp'));
