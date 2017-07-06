1eccea5ff2e1ff8ef04ae1ec3e7229044dc4465e
shahenaz Monia <shahy.m.93@gmail.com>
2017-07-06 10:24:12 +0300

    add provider and connect between container and component

--- src/frontEnd/index.jsx
+++ src/frontEnd/index.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import store from './store.js';
 import { Provider } from 'react-redux';
- import Slider from './components/containers/slideShape.js'
+import SliderShape from './components/containers/slideShape.js';
+import ServicesShape from './components/containers/servicesShape.js';
+import MealsShape from './components/containers/mealsShape.js';
 
-const Main = () => {
- return (
-  <Provider store={store}>
+const Main = ()=>{
+  return (
+    <Provider store={store}>
+      <div>
+        <SliderShape></SliderShape>
+        <ServicesShape></ServicesShape>
+        <MealsShape></MealsShape>
+      </div>
 
-
-      <Slider></Slider>
-    
-      </Provider>
- );
+    </Provider>
+  );
 };
 
+
+
 reactDOM.render(
-<Main/>, document.getElementById('myApp'));
+  <Main/>, document.getElementById('myApp'));
