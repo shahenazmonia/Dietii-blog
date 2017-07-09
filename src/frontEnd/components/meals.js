dd5c210af23ecdccfe12efef37f94a74a4b41df1
shahenaz Monia <shahy.m.93@gmail.com>
2017-07-09 13:28:25 +0300

    add meals images and their style

--- src/frontEnd/components/meals.js
+++ src/frontEnd/components/meals.js
@@ -4,9 +4,16 @@ import React from 'react';
 const meals =({app})=>{
 return(
   <div>
-  <p>dddd</p>
-  </div>
-)
-}
+  <h1 className='mealsTitle'> قائمة الوجبات </h1>
+
+<div className='mealscontainer'>
+<div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-133972145.jpg"></img></div>
+<div className='meals-images'><img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-78329919.jpg"></img></div>
+<div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-114413503.jpg"></img></div>
+<div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-201760443.jpg"></img></div>
+</div>
+</div>
+);
+};
 
 export default meals;
