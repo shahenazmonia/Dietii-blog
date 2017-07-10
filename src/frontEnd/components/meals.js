a9d516fdfa01daadefac2c3a5b3f529319a31097
shahenaz Monia <shahy.m.93@gmail.com>
2017-07-10 16:26:49 +0300

    change class name

--- src/frontEnd/components/meals.js
+++ src/frontEnd/components/meals.js
@@ -3,10 +3,10 @@ import React from 'react';
 
 const meals =({app})=>{
 return(
-  <div>
+  <div className='mealscontainer'>
   <h1 className='mealsTitle'> قائمة الوجبات </h1>
 
-<div className='mealscontainer'>
+<div className='imagescontainer'>
 <div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-133972145.jpg"></img></div>
 <div className='meals-images'><img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-78329919.jpg"></img></div>
 <div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-114413503.jpg"></img></div>
