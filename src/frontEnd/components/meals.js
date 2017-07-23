989d4317fd51158199c4f813c03dfd862940c7b9
shahenaz Monia <shahy.m.93@gmail.com>
2017-07-23 13:24:36 +0300

    refactor code for meals component

--- src/frontEnd/components/meals.js
+++ src/frontEnd/components/meals.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 
-const meals =({app})=>{
-return(
-  <div className='mealscontainer'>
-  <h1 className='mealsTitle'> قائمة الوجبات </h1>
+const meals =()=>{
+  return(
+    <div className='mealscontainer'>
+      <h1 className='mealsTitle'> قائمة الوجبات </h1>
 
-<div className='imagescontainer'>
-<div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-133972145.jpg"></img></div>
-<div className='meals-images'><img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-78329919.jpg"></img></div>
-<div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-114413503.jpg"></img></div>
-<div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-201760443.jpg"></img></div>
-</div>
-</div>
-);
+      <div className='imagescontainer'>
+        <div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-133972145.jpg"></img></div>
+        <div className='meals-images'><img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-78329919.jpg"></img></div>
+        <div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-114413503.jpg"></img></div>
+        <div className='meals-images'>  <img className='mealimage' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/stock-photo-201760443.jpg"></img></div>
+      </div>
+    </div>
+  );
 };
 
 export default meals;
