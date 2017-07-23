b0e1b07683d6043e7f50144575c0dc4f54f3416b
shahenaz Monia <shahy.m.93@gmail.com>
2017-07-23 13:25:17 +0300

    add dietiiStore component

---
+++ src/frontEnd/components/dietiiStore.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const dietiiStore = ()=>{
+  return(
+    <div className='container storeContainer'>
+      <div className='row'>
+        <div className='col-md-offset-2 col-md-6' style={{'text-align': 'right'}}>
+          <img className="googlestore-logo" src="https://raw.githubusercontent.com/shahenazmonia/blog/master/5.png"></img>
+          <p className='googlestore-content-p'>، ، وصفات صحية وغنية، الحصول على وزن مثالي، وصفات صحية وغنية، الحصول على وزن مثاليوصفات صحية وغنية، الحصول على وزن مثاليوصفات صحية وغنية، الحصول على وزن مثالي، وصفات صحية وغنية، الحصول على وزن مثالي، وصفات صحية وغنية، الحصول على وزن مثاليوصفات صحية وغنية، الحصول على وزن مثاليوصفات صحية وغنية، الحصول على وزن مثالي</p>
+          <img className='store-img' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/4.png" ></img>
+        </div>
+        <div className='col-md-4'>
+          <img className="dietii-screenshot" src="https://raw.githubusercontent.com/shahenazmonia/blog/master/sxxz.png"></img></div>
+      </div>
+    </div>
+  );
+};
+
+export default dietiiStore;
