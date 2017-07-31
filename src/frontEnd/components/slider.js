ee1135fd71f9ce7a75e0bd06392518b1faf0d835
shahenaz Monia <shahy.m.93@gmail.com>
2017-07-31 11:26:40 +0300

    edit slider style

--- src/frontEnd/components/slider.js
+++ src/frontEnd/components/slider.js
@@ -4,15 +4,17 @@ import {Nav,Navbar, NavItem} from 'react-bootstrap';
 const slider =  ()=>{
   return(
     <div className='slider-container'>
-      <Navbar className='navigation'>
+      <Navbar className='navigation '>
         <Navbar.Header>
           <Navbar.Brand>
             <a href="#">خدماتنا</a>
           </Navbar.Brand>
         </Navbar.Header>
         <Nav>
-          <NavItem eventKey={1} href="#">واجبات</NavItem>
-          <NavItem eventKey={2} href="#">التطبيق</NavItem>
+          <NavItem eventKey={1} href="#">التطبيق</NavItem>
+          <NavItem eventKey={2} href="#">وجبات</NavItem>
+          <NavItem eventKey={3} href="#">المدونة</NavItem>
+          <NavItem eventKey={4} href="#">تواصل معنا</NavItem>
         </Nav>
       </Navbar>
       <img className='slider-img' src="https://raw.githubusercontent.com/shahenazmonia/blog/master/6.png" ></img>
