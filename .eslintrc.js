6a4985f20ad57eb475b2b72ae8d96ff268135c20
shahenaz Monia <shahy.m.93@gmail.com>
2018-01-09 20:47:20 +0200

    refactor eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,67 @@
 module.exports = {
 
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "node": true,
-        "jest": true,
-        "es6": true
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "node": true,
+    "jest": true,
+    "es6": true
   },
-    "extends": ["eslint:recommended", "plugin:react/recommended"],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
+  "extends": [
+    "eslint:recommended", "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+      "experimentalObjectRestSpread": true
     },
-    "plugins": [
-        "react"
+    "sourceType": "module"
+  },
+  "plugins": ["react"],
+  "rules": {
+    "indent": [
+      "error", 2
     ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    },
-    "globals": {
-      gapi: true
-    }
+    "linebreak-style": [
+      "error", "unix"
+    ],
+    "quotes": [
+      "error", "single"
+    ],
+    "semi": [
+      "error", "always"
+    ],
+    "arrow-parens": [
+      2,
+      "as-needed", {
+        "requireForBlockBody": true
+      }
+    ],
+    "object-curly-newline": [
+      "error", {
+        "consistent": true
+      }
+    ],
+    "object-curly-spacing": [
+      "error",
+      "never", {
+        "objectsInObjects": true
+      }
+    ],
+    "max-len": [
+      2,
+      80,
+      4, {
+        "ignoreUrls": true
+      }
+    ],
+    "react/display-name": [
+      0, {
+        "ignoreTranspilerName": true
+      }
+    ]
+  },
+  "globals": {
+    gapi: true
+  }
 }
