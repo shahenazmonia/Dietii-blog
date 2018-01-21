df507aa6ee5d09dd06c658bdb899104e1f02978e
shahenaz Monia <shahy.m.93@gmail.com>
2018-01-21 21:12:12 +0200

    create component for a meal

---
+++ src/frontEnd/components/meal.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class meal extends Component {
+
+  render(){
+    console.log('dddddddddd');
+    return(
+      <div><h1>single page </h1></div>
+    );
+  }
+}
+
+export default meal;
