cab8b6ae3423cba573a1d770aafdbfaa06e253ef
shahenaz Monia <shahy.m.93@gmail.com>
2018-01-21 21:15:18 +0200

    delete unnecessary modules

--- src/frontEnd/store.js
+++ src/frontEnd/store.js
@@ -1,5 +1,4 @@
 import {createStore,applyMiddleware} from 'redux';
-import {routerMiddleware, routerReducer, syncHistoryWithStore} from'react-router-redux';
 import ReduxThunk from 'redux-thunk';
 import rootReducers from './reducers';
 
