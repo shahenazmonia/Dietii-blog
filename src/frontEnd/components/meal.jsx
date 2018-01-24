e86427e27bc90354f40b1fb03ce770ffd89d4903
shahenaz Monia <shahy.m.93@gmail.com>
2018-01-24 15:56:34 +0200

    return meal data from mealAction

--- src/frontEnd/components/meal.jsx
+++ src/frontEnd/components/meal.jsx
@@ -1,12 +1,51 @@
 import React, {Component} from 'react';
-
+import Header from './containers/header.js';
+import Meals from './containers/meals.js';
+import Footer from './containers/footerShape.js';
 class meal extends Component {
+  componentDidMount() {
+    const {fetchMeal} = this.props;
+    const objectId= this.props.match.params.id;
+    fetchMeal(objectId);
+  }
+  componentWillReceiveProps(){
+    const {meal} = this.props;
 
+  }
   render(){
-    console.log('dddddddddd');
+    const {meal} = this.props;
     return(
-      <div><h1>single page </h1></div>
-    );
+      <div>
+        <Header></Header>
+        <div className='container'>
+
+          {
+            (() => {
+              if (meal.meal.image){
+                return (<div className='col-md-5'>
+                  <img className='meal-image' src={meal.meal.image.url}  key={meal.meal.image}/>
+                </div>);
+              }else {
+                return null;
+              }})()
+
+          }
+
+          <div className='col-md-7 meal-content'>
+            <h2>{meal.meal.name}</h2>
+            <p className='meal-description'>{meal.meal.resipe}</p>
+            <a className='meal-app-link' hreff ='https://play.google.com/store/apps/details?id=com.dietiiApp.dietiiApp&hl=ar '> الوجبة داخل التطبيق</a>
+          </div>
+
+        </div>
+        <div>
+          <Meals></Meals>
+          <div className='meal-footer'> <Footer></Footer></div>
+        </div>
+      </div>);
+
+
+
   }
 }
 
