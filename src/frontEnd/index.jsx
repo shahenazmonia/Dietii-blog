811bc7bc07f3827dbef1804872c7f75762e7be92
shahenaz Monia <shahy.m.93@gmail.com>
2018-01-24 15:57:00 +0200

    refactor index code for app

--- src/frontEnd/index.jsx
+++ src/frontEnd/index.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
 import reactDOM from 'react-dom';
 import store from './store.js';
-import { Provider } from 'react-redux';
-import SliderShape from './components/containers/slideShape.js';
-import ServicesShape from './components/containers/servicesShape.js';
-import MealsShape from './components/containers/meals.js';
-import BlogShape from './components/containers/blogsShape.js';
-import DietiiStore from './components/containers/dietiiStoreShape.js';
-import DietiiTeamShape from './components/containers/dietiiTeamShape.js';
-import InstagramShape from './components/containers/instagramShape.js';
-import FooterShape from './components/containers/footerShape.js';
+import {Provider} from 'react-redux';
 
-
+import App from './components/App';
 
 const Main = ()=>{
   return (
     <Provider store={store}>
-      <div>
-        <SliderShape></SliderShape>
-        <ServicesShape></ServicesShape>
-        <MealsShape></MealsShape>
-        <BlogShape></BlogShape>
-        <DietiiStore></DietiiStore>
-        <DietiiTeamShape></DietiiTeamShape>
-        <InstagramShape></InstagramShape>
-        <FooterShape></FooterShape>
-      </div>
-
+      <App />
     </Provider>
   );
 };
