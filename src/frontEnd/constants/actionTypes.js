5b734be237eb10a520d06d14f1ced59409ca550f
shahenaz Monia <shahy.m.93@gmail.com>
2018-01-28 14:26:34 +0200

    create constants for static variables

---
+++ src/frontEnd/constants/actionTypes.js
@@ -0,0 +1,6 @@
+export const getMeals = 'getMeals';
+export const getMealsSuccess = 'getMealsSuccess';
+export const getMealsFail = 'getMealsFail';
+export const getMeal = 'getMeal';
+export const getMealSuccess = 'getMealsSuccess';
+export const getMealFail = 'getMealsFail';
